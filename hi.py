c4a9049f0ccabc8f2803a989cc5856e5ef905ed2
Sahil channa <122665949+isahilchanna@users.noreply.github.com>
2023-03-11 18:04:55 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -3,10 +3,10 @@ import time
 i=0
 
 while not i>=100:
-    py.typewrite("FUCKU")
+    py.typewrite("{}")# put your thing that you wanna send !!
     py.press("enter")
     i+=1
     inputt=str(input())
-    if inputt =='mf':
+    if inputt =='{}':# anything
         break
     py.FAILSAFE=False
